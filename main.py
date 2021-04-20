1c7f217b9a9025566b5330fbab239f6379444d7a
Rblba20 <hp5722*2005ML>
2021-04-20 20:49:43 +0500

    first form

--- main.py
+++ main.py
@@ -19,5 +19,17 @@ def test():
     return render_template('test.html')
 
 
+@app.route('/add_phone', methods=['POST', 'GET'])
+def add_phone():
+    if request.method == 'GET':
+        return render_template('add_phone.html')
+    elif request.method == 'POST':
+        print(request.form['mobile'])
+        print(request.form['message'])
+        print(request.form['datetime-local'])
+        print(request.form['regions'])
+        return "Форма отправлена"
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
