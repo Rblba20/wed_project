59f323bdec59451706360eec8291eaca01c2125b
Rblba20 <74965746+Rblba20@users.noreply.github.com>
2021-04-29 18:01:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -236,4 +236,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port
